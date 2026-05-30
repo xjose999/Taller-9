f080e2c92690fec2dfa8e322dcec3812c84122d4
xjose999 <josejoestar80@gmail.com>
2026-05-29 23:06:49 -0500

    Ejercicio 2

---
+++ Animal.java
@@ -0,0 +1,11 @@
+class Animal {
+    String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+}
