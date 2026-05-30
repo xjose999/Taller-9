44c8ad8b1457232708b1b807c808c1b8481c94c3
xjose999 <josejoestar80@gmail.com>
2026-05-29 22:36:00 -0500

    Ejercicio 1

---
+++ Persona.java
@@ -0,0 +1,9 @@
+class Persona {
+    String nombre;
+    int edad;
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
